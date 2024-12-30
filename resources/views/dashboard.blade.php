b673ba4f12c6bc4ec1192c362f7a1a3a6ed2da83
SarangaBRanasinghe <ranasingherpksb.22@uom.lk>
2024-12-30 09:56:37 +0530

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,25 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    dashboard
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+            <div class="p-6 text-center text-gray-900 dark:text-gray-100">
+                <img 
+                    src="https://png.pngtree.com/png-clipart/20230715/original/pngtree-proud-new-car-owner-land-vehicle-success-car-vector-png-image_9304726.png" 
+                    alt="car owner" 
+                    class="w-full max-w-md mx-auto mb-4 rounded-lg"
+                >
+                <div class="mb-4">
+                    <p>Register your vehicle by clicking the button below:</p>
                 </div>
+                <a href="{{ route('vehicle.index') }}" 
+                   class="px-4 py-2 mx-auto text-white transition bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">
+                    Click Here
+                </a>
             </div>
         </div>
     </div>
+</div>
+
 </x-app-layout>
